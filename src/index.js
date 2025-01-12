c7fa10b87cb0b7cc3b4442bce1940dc7f8a014b0
Balaji <20x51a0483@srecnandyal.edu.in>
2025-01-12 16:57:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom"; // Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+root.render(<App />);
